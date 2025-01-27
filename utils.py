a4333413ce768baa5c733313ad3284a1c6226ddf
vicbguti <284-vicbguti@users.noreply.gitlab.espol.edu.ec>
2025-01-26 21:09:41 -0500

    update tweets

--- utils.py
+++ utils.py
@@ -9,19 +9,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def clean_url(url: str) -> str:
-    return url.split('?')[0]
+    return url.split("?")[0]
 
-def build_url_search(url_base, keywords = []):
-  url = f'{url_base}?q='
 
-  for keyword in keywords:
-    url += f'{keyword}%20'
-  
-  return url
+def build_url_search(url_base, keywords=[]):
+    url = f"{url_base}?q="
+
+    for keyword in keywords:
+        url += f"{keyword}%20"
+
+    return url
+
 
 def parse_text_number(text: str) -> int:
-    suffixes = {'k': 1_000, 'm': 1_000_000, 'b': 1_000_000_000}
+    suffixes = {"k": 1_000, "m": 1_000_000, "b": 1_000_000_000}
 
     if text is None or len(text) == 0:
         return 50
@@ -32,7 +35,7 @@ def parse_text_number(text: str) -> int:
             number = float(text[:-1])
             multiplier = suffixes[suffix_text]
             return int(number * multiplier)
-        
+
         return int(text)
     except ValueError:
         return 50
@@ -40,21 +43,26 @@ def parse_text_number(text: str) -> int:
 
 def clean_text(text: str) -> str:
     if text is None:
-        return ''
+        return ""
 
-    return text.strip().replace('\n', '').replace('\t', '')
+    return text.strip().replace("\n", "").replace("\t", "")
+
 
 class SearcherDriver:
-    def __init__(self, executable_path = None, check_captcha_fn = None):
+    def __init__(self, executable_path=None, check_captcha_fn=None):
         if executable_path is None:
-            print('[WARNING] No executable path provided. Searching for chromedriver in PATH')
+            print(
+                "[WARNING] No executable path provided. Searching for chromedriver in PATH"
+            )
 
-        #self.file = file
+        # self.file = file
         self.check_captcha_fn = check_captcha_fn
         self.by_type = {"id": By.ID, "css": By.CSS_SELECTOR, "xpath": By.XPATH}
-        self.driver = webdriver.Chrome(service=webdriver.ChromeService(executable_path=executable_path))
+        self.driver = webdriver.Chrome(
+            service=webdriver.ChromeService(executable_path=executable_path)
+        )
 
-        #self.url_scraped = self.load_url_scraped()
+        # self.url_scraped = self.load_url_scraped()
 
     def set_file(self, file: str, city: str):
         self.file = file
@@ -62,9 +70,9 @@ class SearcherDriver:
         self.url_scraped = self.load_url_scraped()
 
     def append_data(self, data: str):
-        with open(self.file, 'a') as f:
-            f.write(f'{data}\t{self.city}\n')
-            self.url_scraped.append(clean_url(data.split(',')[0]))
+        with open(self.file, "a") as f:
+            f.write(f"{data}\t{self.city}\n")
+            self.url_scraped.append(clean_url(data.split(",")[0]))
 
     def is_url_scraped(self) -> bool:
         result = clean_url(self.driver.current_url) in self.url_scraped
@@ -76,14 +84,16 @@ class SearcherDriver:
     def load_url_scraped(self: str):
         if not os.path.exists(self.file):
             print(f"[DRIVER] File not found: {self.file} - Creating new file")
-            with open(self.file, 'w') as f:
-                f.write('link	description	username	date	links	comments	shares	comments_text	city\n')
+            with open(self.file, "w") as f:
+                f.write(
+                    "link	description	username	date	links	comments	shares	comments_text	city\n"
+                )
             return []
 
-        with open(self.file, 'r') as f:
+        with open(self.file, "r") as f:
             lines = f.readlines()
 
-            urls = [line.split('\t')[0] for line in lines]
+            urls = [line.split("\t")[0] for line in lines]
             print(f"[DRIVER] Scraped URLs: {len(urls)}")
 
             return urls
@@ -94,22 +104,28 @@ class SearcherDriver:
     def close(self):
         self.driver.close()
 
-    def wait(self, timeout = 5):
+    def wait(self, timeout=5):
         WebDriverWait(self.driver, timeout)
 
-    def random_sleep(self, min_v = 1, max_v = 5):
+    def random_sleep(self, min_v=1, max_v=5):
         seconds = random.randint(min_v, max_v)
         print(f"[DRIVER] Sleeping for {seconds} seconds")
         time.sleep(seconds)
 
-    def get_element_by(self, selector_type: Literal['id', 'css', 'xpath'], selector: str, timeout = 40, from_capcha=False) -> WebElement | None:
+    def get_element_by(
+        self,
+        selector_type: Literal["id", "css", "xpath"],
+        selector: str,
+        timeout=40,
+        from_capcha=False,
+    ) -> WebElement | None:
         by_selector = self.by_type.get(selector_type)
 
         if by_selector is None:
             raise ValueError(
                 f"Invalid selector type: {selector_type} - must be 'id', 'css', or 'xpath'"
             )
-        
+
         if self.check_captcha_fn is not None and not from_capcha:
             self.check_captcha_fn(self)
 
@@ -129,15 +145,20 @@ class SearcherDriver:
 
         return element
 
-
-    def get_elements_by(self, selector_type: Literal['id', 'css', 'xpath'], selector: str, timeout = 40, from_capcha=False) -> List[WebElement]:
+    def get_elements_by(
+        self,
+        selector_type: Literal["id", "css", "xpath"],
+        selector: str,
+        timeout=40,
+        from_capcha=False,
+    ) -> List[WebElement]:
         by_selector = self.by_type.get(selector_type)
 
         if by_selector is None:
             raise ValueError(
                 f"Invalid selector type: {selector_type} - must be 'id', 'css', or 'xpath'"
             )
-        
+
         if self.check_captcha_fn is not None and not from_capcha:
             self.check_captcha_fn(self)
 
@@ -157,7 +178,7 @@ class SearcherDriver:
             return []
 
         return elements
-    
+
     def run(self, run_fn: Callable):
         try:
             print(f"[DRIVER] Running {run_fn.__name__}")
